0f17528a3418f3de3de43fb8c42b39daee17785b
yogeshgarg0011 <125199173+yogeshgarg0011@users.noreply.github.com>
2023-05-05 09:20:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVariable
+{
+    internal class AreaOfCircle
+    {
+        double area;
+        int radius;
+        const double pie = 3.14; 
+
+        public void calculateArea(double r)
+        {
+            area = r * r * 3.14;
+            Console.WriteLine("area of circle {0} which radius is {1}",area,r);
+        }
+        static void Main(string[] args)
+        {
+            AreaOfCircle c1= new AreaOfCircle();
+            Console.WriteLine("enter radius");
+            int R=Convert.ToInt32(Console.ReadLine());
+            c1.calculateArea(R);
+            Console.ReadLine();
+        }
+      
+    }
+}
